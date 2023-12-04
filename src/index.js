9a277c190963eb86d2901d985a26ac00d83a8659
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2023-12-04 22:29:56 +0300

    Another changing of homepage address and router...

--- src/index.js
+++ src/index.js
@@ -2,21 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { RouterProvider, createHashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
-
-const router = createHashRouter([
-	{
-	  path: "/",
-	  element: <App />,
-	}
-  ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
 	<React.StrictMode>
-		<RouterProvider router={ router }/>
+		<HashRouter>
+			<App />
+		</HashRouter>
 	</React.StrictMode>
 );
